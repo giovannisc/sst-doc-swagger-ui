54be2538011cca01dbad6f52339ad61cdc12976a
Giovanni Schimidt <giovanni.schimidt@gmail.com>
2023-12-16 22:47:38 -0300

    change config container health check

--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -5,21 +5,21 @@ export function DOC({ stack, app }: StackContext) {
   const service = new Service(stack, "MyService", {
     path: ".",
     file: "./services/Dockerfile",
-    port: 80,
+    port: 3002,
     cdk: {
       container: {
         healthCheck: {
-          command: ["CMD-SHELL", "curl -f http://localhost || exit 1"],
-          interval: Duration.minutes(1),
-          retries: 2,
-          startPeriod: Duration.minutes(1),
-          timeout: Duration.seconds(30),
+          command: ["CMD-SHELL", "curl -f http://localhost:3002/health || exit 1"],
+          interval: Duration.minutes(2),
+          retries: 3,
+          startPeriod: Duration.minutes(5),
+          timeout: Duration.minutes(1),
         },
       },
       applicationLoadBalancerTargetGroup: {
         healthCheck: {
           healthyHttpCodes: "200,301,302",
-          path: "/",
+          path: "/health",
         },
       }
     },
